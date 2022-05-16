a37dcea91939a6848f28e15386ed3f961adf5afd
danahAlajmi <danah38x@gmail.com>
2022-05-16 20:45:27 +0300

    DONE

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import './App.css';
-import Nav from './components/Nav';
-import Home from './components/Home';
-import TripsList from './components/TripsList';
-import TripDetail from './components/TripDetail';
-
+import "./App.css";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import TripsList from "./components/TripsList";
+import TripDetail from "./components/TripDetail";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div id="page-top">
       <Nav />
-      <Home />
-      <TripsList />
-      <TripDetail />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/trips-List" element={<TripsList />} />
+        <Route path="/trip-Detail" element={<TripDetail />} />
+        <Route path="/trip-Detail/:tripSlug" element={<TripDetail />} />
+      </Routes>
     </div>
   );
 }
